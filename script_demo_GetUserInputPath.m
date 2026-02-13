88ee050041fd31e26c3232e8fcb4bdde2aa7de65
Intelligent Vehicles and Systems Group (Prof. Sean Brennan) <67085752+ivsg-psu@users.noreply.github.com>
2026-02-12 23:39:19 -0500

    Update script_demo_GetUserInputPath.m

--- script_demo_GetUserInputPath.m
+++ script_demo_GetUserInputPath.m
@@ -19,9 +19,9 @@
 % 
 % 2026_02_12 by Sean Brennan, sbrennan@psu.edu
 % - In script_demo_GetUserInputPath
-%   % * First creation of the repo
+%   % * First creation of the repo in standard form
 %   % * Added automatic release check section
-
+% (new release)
 
 % TO-DO:
 % - 2026_02_12 by Sean Brennan, sbrennan@psu.edu
